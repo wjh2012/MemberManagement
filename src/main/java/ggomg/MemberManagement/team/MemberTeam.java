2103fa4a89af48748ae22961e1af23806be36716
wjh2012 <wjh2012@naver.com>
2023-09-09 18:05:15 +0900

    Member, Team 다대다 매핑 테이블

---
+++ src/main/java/ggomg/MemberManagement/team/MemberTeam.java
@@ -0,0 +1,34 @@
+package ggomg.MemberManagement.team;
+
+import ggomg.MemberManagement.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberTeam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn
+    private Team team;
+}
