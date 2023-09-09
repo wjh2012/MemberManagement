1a0de73a79c21630172ceacac0b78d9bd3adfaf1
wjh2012 <wjh2012@naver.com>
2023-09-09 22:50:50 +0900

    update flyway script

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,32 +1,27 @@
 CREATE TABLE member
 (
-    id               BIGINT AUTO_INCREMENT,
-    is_oauth         bit,
+    id               BIGINT AUTO_INCREMENT PRIMARY KEY,
+    is_oauth         TINYINT(1),
     auth_type        VARCHAR(255),
     oauth_id         VARCHAR(255),
     nickname         VARCHAR(255),
     username         VARCHAR(255),
     password         VARCHAR(255),
-    created_datetime datetime(6),
-
-    PRIMARY KEY (id)
+    created_datetime DATETIME
 );
 
 CREATE TABLE role
 (
-    id        BIGINT AUTO_INCREMENT,
-    role_name VARCHAR(255),
-
-    PRIMARY KEY (id)
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(255)
 );
 
 CREATE TABLE member_role
 (
-    id         BIGINT AUTO_INCREMENT,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     member_id  BIGINT,
     role_id    BIGINT,
-    grant_date datetime(6),
-
-    PRIMARY KEY (id)
+    grant_date DATETIME,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
-
