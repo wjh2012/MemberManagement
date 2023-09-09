78c1849f0d1ef9a66bc6794154603bbd1651abfa
wjh2012 <wjh2012@naver.com>
2023-09-09 17:57:03 +0900

    회원가입 폼 제약조건 추가

--- src/main/java/ggomg/MemberManagement/controller/DTO/request/LocalMemberRegisterRequest.java
+++ src/main/java/ggomg/MemberManagement/controller/DTO/request/LocalMemberRegisterRequest.java
@@ -1,18 +1,26 @@
 package ggomg.MemberManagement.controller.DTO.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class LocalMemberRegisterRequest {
+
     @NotNull
+    @Size(min = 4, max = 16)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String username;
+
     @NotNull
+    @Size(min = 8, max = 32)
     private String password;
+
     @NotNull
+    @Size(min = 1, max = 16)
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$")
     private String nickname;
-
-
 }
