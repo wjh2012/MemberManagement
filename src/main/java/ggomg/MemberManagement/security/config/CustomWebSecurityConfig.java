b95e5c5c85c33f78148e40d17ae4ac4dea12657e
wjh2012 <wjh2012@naver.com>
2023-09-18 20:49:44 +0900

    add web.ignoring(css,js)

--- src/main/java/ggomg/MemberManagement/security/config/CustomWebSecurityConfig.java
+++ src/main/java/ggomg/MemberManagement/security/config/CustomWebSecurityConfig.java
@@ -1,37 +1,49 @@
 package ggomg.MemberManagement.security.config;
 
 
+import ggomg.MemberManagement.disabledMember.DisableMemberRepository;
+import ggomg.MemberManagement.security.CheckDisabledUserFilter;
 import ggomg.MemberManagement.security.OAuth2User.CustomAuthorizationRequestResolver;
 import ggomg.MemberManagement.security.OAuth2User.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = false)
 @RequiredArgsConstructor
 @Slf4j
 public class CustomWebSecurityConfig {
 
     private final CustomAuthorizationRequestResolver customAuthorizationRequestResolver;
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final DisableMemberRepository disableMemberRepository;
+
+    @Bean
+    public WebSecurityCustomizer ignoringCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterAfter(new CheckDisabledUserFilter(disableMemberRepository), SecurityContextHolderFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/oauth2/**", "/login/**", "/register/**", "/normal/**", "/error/**"
-                                , "/css/**", "/images/**", "/js/**", "/console/**", "/favicon.ico/**")
+                                , "/images/**", "/console/**", "/favicon.ico/**")
                         .permitAll()
                         .requestMatchers("/member/oauth2").hasAuthority("OAUTH2_USER")
                         .requestMatchers("/member/oidc").hasAuthority("OIDC_USER")
