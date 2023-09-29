7a963954af3a1ffc2282e0873ba84eddda4bd130
wjh2012 <wjh2012@naver.com>
2023-09-30 00:20:47 +0900

    update security config

--- src/main/java/ggomg/MemberManagement/API/config/APIWebSecurityConfig.java
+++ src/main/java/ggomg/MemberManagement/API/config/APIWebSecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.session.CompositeSessionAuthenticationStrategy;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,47 +32,24 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class APIWebSecurityConfig {
 
-    private final PasswordEncoder passwordEncoder;
-    private final CustomUserDetailsService customUserDetailsService;
-    private final APIAuthenticationSuccessHandler successHandler;
-    private final APIAuthenticationFailureHandler failureHandler;
-
-    public AuthenticationManager authenticationManager() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(customUserDetailsService);
-        return new ProviderManager(provider);
-    }
-
-    JSONUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() {
-
-        JSONUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter = new JSONUsernamePasswordAuthenticationFilter();
-        jsonUsernamePasswordAuthenticationFilter.setFilterProcessesUrl("/api/login");
-        jsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
-        jsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(successHandler);
-        jsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(failureHandler);
-        return jsonUsernamePasswordAuthenticationFilter;
-    }
+    private final APIAuthenticationSuccessHandler apiAuthenticationSuccessHandler;
 
     @Bean
     @Order(1)
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .sessionManagement(m -> m
-                        .addObjectPostProcessor(new ObjectPostProcessor<CompositeSessionAuthenticationStrategy>() {
-                            @Override
-                            public <O extends CompositeSessionAuthenticationStrategy> O postProcess(O object) {
-                                jsonUsernamePasswordAuthenticationFilter().setSessionAuthenticationStrategy(object);
-                                return object;
-                            }
-                        }))
                 .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login/**")
                         .permitAll())
-                .addFilterBefore(jsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .formLogin(form->form
+                        .loginProcessingUrl("/api/login")
+                        .successHandler(apiAuthenticationSuccessHandler)
+                )
+
         ;
         return http.build();
     }
@@ -78,9 +58,10 @@ public class APIWebSecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://ggomg.net"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
---
+++ src/main/java/ggomg/MemberManagement/API/controller/ApiController.java
@@ -0,0 +1,7 @@
+package ggomg.MemberManagement.API.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class ApiController {
+}
